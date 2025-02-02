d390ae1f708337159bb318c253f0d1465f3a1536
Student <Student@example.com>
2025-02-02 18:56:01 +0330

    RegisterationForm

---
+++ Models/UserRegistration.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventEasePro.Models
+{
+    public class UserRegistration
+    {
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(50, ErrorMessage = "Username can't be longer than 50 characters")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, ErrorMessage = "Password must be at least 6 characters", MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+}
---
+++ Services/AuthenticationService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace EventEasePro.Services
+{
+    public class AuthenticationService
+    {
+        private string _user;
+
+        public bool IsUserLoggedIn => !string.IsNullOrEmpty(_user);
+
+        public string CurrentUser => _user;
+
+        public void Login(string username)
+        {
+            _user = username;
+        }
+
+        public void Logout()
+        {
+            _user = null;
+        }
+    }
+}
