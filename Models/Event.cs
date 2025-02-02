904e005b1afa37f14a4bc2d86d28d9d1d8e66e65
Student <Student@example.com>
2025-02-02 18:33:25 +0330

    Add project files.

---
+++ Models/Event.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventEasePro.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }  // Primary key
+        [Required(ErrorMessage ="Name is Required!")]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+    }
+}
---
+++ Services/EventService.cs
@@ -0,0 +1,45 @@
+﻿using EventEasePro.Models;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class EventService
+{
+    private ConcurrentDictionary<int, Event> _events = new ConcurrentDictionary<int, Event>();
+    private int _currentId = 1;
+
+    public Task<List<Event>> GetEventsAsync()
+    {
+        return Task.FromResult(_events.Values.ToList());
+    }
+
+    public Task<Event> GetEventAsync(int id)
+    {
+        _events.TryGetValue(id, out var ev);
+        return Task.FromResult(ev);
+    }
+
+    public Task AddEventAsync(Event newEvent)
+    {
+        newEvent.Id = _currentId++;
+        _events[newEvent.Id] = newEvent;
+        return Task.CompletedTask;
+    }
+
+    public Task UpdateEventAsync(Event updatedEvent)
+    {
+        if (_events.ContainsKey(updatedEvent.Id))
+        {
+            _events[updatedEvent.Id] = updatedEvent;
+        }
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteEventAsync(int id)
+    {
+        _events.TryRemove(id, out _);
+        return Task.CompletedTask;
+    }
+}
+
